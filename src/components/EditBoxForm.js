b6f6a3d55df23230d9b104ba4a9fc32a70a4d17c
Eva Kemp <eva.j.kemp@gmail.com>
2023-09-10 16:02:57 -0700

    update edit box form

---
+++ src/components/EditBoxForm.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReusableForm from "./ReusableForm";
+import PropTypes from "prop-types";
+
+function EditBoxForm(props) {
+  const { box } = props;
+
+  function handleEditBoxFormSubmission(event) {
+    event.preventDefault();
+    props.onEditBox({
+      color: event.target.color.value,
+      origin: event.target.origin.value,
+      price: event.target.price.value,
+      id: box.id,
+    });
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleEditBoxFormSubmission}
+        buttonText="Update Box"
+      />
+    </React.Fragment>
+  );
+}
+
+EditBoxForm.propTypes = {
+  onEditBox: PropTypes.func,
+  box: PropTypes.object,
+};
+
+export default EditBoxForm;
